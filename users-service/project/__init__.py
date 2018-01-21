08bde826e60b8641a50d18bc11ff5a0f95b83911
Alisson Moura <alisson.belas@gmail.com>
2018-01-21 12:40:33 -0200

    adding bluprint for users

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,36 +1,29 @@
 # users-service/project/__init__.py
 
+
 import os
-import datetime
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
+# instantiate the db
+db = SQLAlchemy()
 
-# instantiate the app
-app = Flask(__name__)
 
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+def create_app():
 
-db = SQLAlchemy(app)
+    # instantiate the app
+    app = Flask(__name__)
 
-# model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # set up extensions
+    db.init_app(app)
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    return app
